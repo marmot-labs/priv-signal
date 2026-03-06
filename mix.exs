d17b8d11ceab364e190df543e0165acaaa3908f8
Darren Siegel <siegel.darren@gmail.com>
2026-03-06 13:10:36 -0500

    set version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PrivSignal.MixProject do
   def project do
     [
       app: :priv_signal,
-      version: "0.1.0",
+      version: "0.2.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps()
